50ea73b17164721c154cce553940c96484e26aa0
Sarah <sarah.saker@hotmail.fr>
2024-07-23 18:55:33 +0200

    Init front react redux

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+import '../styles/globals.css';
+import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+
+const store = configureStore({
+  reducer: { users },
+ });
+
+function App({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <Head>
+        <title>Twitter</title>
+      </Head>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
+
+export default App;
